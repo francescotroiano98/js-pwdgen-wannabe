0fcc2dc737c6b4e9f712a7bfe9be6e8848fa710c
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-11 16:18:03 +0200

    Done password generator

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const nome = prompt("scrivi il tuo nome");
+console.log(nome);
+const cognome = prompt("scrivi il tuo cognome");
+console.log(cognome);
+const colore = prompt("scrivi il tuo colore preferito");
+console.log(colore);
+const anno = 23
+
+document.getElementById("nomecognomecoloreanno").innerHTML = nome + cognome + colore + anno
+console.log(nome + cognome + colore + anno);
